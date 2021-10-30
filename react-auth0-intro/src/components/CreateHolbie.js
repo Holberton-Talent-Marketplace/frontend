e5076d13dd7cc36906e989643cb368115418be80
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-10-30 10:51:35 -0500

    create routes, add navigation bar and add loging auth0

---
+++ react-auth0-intro/src/components/CreateHolbie.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CreateHolbie extends Component {
+	render() {
+		return (
+			<div>
+				Form to create Holbie profile
+			</div>
+		)
+	}
+}
