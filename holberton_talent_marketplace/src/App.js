7efdb83cb11c202f212e96a0c8602bef24c0c6aa
Juansu01 <thejuank0@hotmail.com>
2021-10-31 08:41:36 -0500

    Adding logout button to navbar

--- holberton_talent_marketplace/src/App.js
+++ holberton_talent_marketplace/src/App.js
@@ -1,5 +1,5 @@
 import React, { component } from 'react';
-import { BrowserRouter as Router , Route, link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, link } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import './App.css';
@@ -19,21 +19,50 @@ import CreateHolbie from './components/CreateHolbie'
 import CreateCompany from './components/CreateCompany'
 import CompanyProfile from './components/CompanyProfile'
 import Home from './components/Home'
-
-
+import { Link } from 'react-router-dom'
 
 function App() {
 	/*
 	const { isAuthenticated }= useAuth0()
 */
-	const { isAuthenticated }= useAuth0()
+	const { isAuthenticated } = useAuth0()
 	return (
 		<Router>
-			<Navigation/>
-			{
-			isAuthenticated ? <LogoutButton/> : <LoginButton/>
-			}
-			<Profile/>
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+				<div className="container">
+					<Link className="navbar-brand" to="/">
+						<img src={require("./logos/Logo peque gris.PNG").default} width="50" height="30" alt="" />
+						Holberton Talent Marketplace
+					</Link>
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarNav">
+						<ul class="navbar-nav ms-auto">
+							<li class="nav-item active">
+								<Link className="nav-link" to="/">Home</Link>
+							</li>
+							<li class="nav-item">
+								<Link className="nav-link" to="/listholbies">Holbies</Link>
+							</li>
+							<li class="nav-item">
+								<Link className="nav-link" to="/listcompanies">Companies</Link>
+							</li>
+							<li class="nav-item">
+								<Link className="nav-link" to="/createholbie">Create Holbie</Link>
+							</li>
+							<li class="nav-item">
+								<Link className="nav-link" to="/createcompany">Create company</Link>
+							</li>
+							<li class="nav-item">
+								{
+									isAuthenticated ? <LogoutButton /> : <LoginButton />
+								}
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
 			<Route exact path="/" component={Home} />
 			<Route path="/listholbies" component={ListHolbies} />
 			<Route path="/listcompanies" component={ListCompanies} />
@@ -45,16 +74,16 @@ function App() {
 
 		</Router>
 
-/*
-	<div className="App">
-		<h1>Holberton Talent Marketplace</h1>
-		{
-			isAuthenticated ? <LogoutButton/> : <LoginButton/>
-		}
-		<Profile/>
-    </div>
-*/
-  );
+		/*
+			<div className="App">
+				<h1>Holberton Talent Marketplace</h1>
+				{
+					isAuthenticated ? <LogoutButton/> : <LoginButton/>
+				}
+				<Profile/>
+			</div>
+		*/
+	);
 };
 
 export default App;
--- holberton_talent_marketplace/src/components/ListHolbies.js
+++ holberton_talent_marketplace/src/components/ListHolbies.js
@@ -35,46 +35,50 @@ const ListHolbies = () => {
             ),
         );
     }
-
-    function capitalizeFirstLetter(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
+    function setImage(holbieId) {
+        try {
+            return require("../profile_pitures/" + holbieId + ".png")
+        } catch (error) {
+            return require("../profile_pitures/default_avatar.jpg")
+        }
     }
-
     const columns = holbies[0] && Object.keys(holbies[0]);
     console.log(columns)
     return <Fragment>
         <div class="container">
-        <h1 className="text-center mt-5">Find A Holbie</h1>
-        <form>
-            <input type="text" className="form-control form-control-sm mt-5" placeholder="Name, technologies, gender" value={q} onChange={(e) => setQ(e.target.value)}></input>
-        </form>
-        <section class="section about-section gray-bg" id="about">
-            <div class="container border">
-                {search(holbies).map(holbie => (
-                    <div class="flex-row-reverse border-bottom border-top">
-                        <div class="">
-                            <div class="about-text go-to">
-                                <img src={require("../profile_pitures/" + holbie.id + ".png").default } class="img-thumbnail rounded float-end" width="200" height="200" onError={(e)=>{e.target.onerror = null; e.target.src=require("../profile_pitures/02B8s9VcZQ.png").default}} /> 
-                                <h3 class="dark-color mt-4">{holbie.name}</h3>
-                                <p>{holbie.about_me.substring(0, 200) + "..."}</p>
-                            </div>
-                            <div class="about-text go-to">
-                                <h4 class="dark-color">Technologies</h4>
-                                <p>{holbie.technologies}</p>
-                            </div>
-                            <div class="about-text go-to">
-                                <h4 class="dark-color">The most amazing thing</h4>
-                                <p>{holbie.most_amazing_thing}</p>
-                            </div>
-                            <div class="about-text go-to">
-                                <h4 class="dark-color">Gender</h4>
-                                <p>{holbie.gender}</p>
+            <h1 className="text-center mt-5">Find A Holbie</h1>
+            <form>
+                <input type="text" className="form-control form-control-sm mt-5" placeholder="Name, technologies, gender" value={q} onChange={(e) => setQ(e.target.value)}></input>
+            </form>
+            <section class="section about-section gray-bg" id="about">
+                <div class="container border">
+                    {search(holbies).map(holbie => (
+                        <div class="flex-row-reverse border-bottom border-top">
+                            <div class="">
+                                <div class="about-text go-to">
+                                    <img src={require("../profile_pitures/" + holbie.id + ".png").default} class="img-thumbnail rounded float-end" width="200" height="200" onError={(e) => {
+                                        e.target.src = '../profile_pictures/default_avatar.jpg' // some replacement image
+                                    }} />
+                                    <h3 class="dark-color mt-4">{holbie.name}</h3>
+                                    <p>{holbie.about_me.substring(0, 200) + "..."}</p>
+                                </div>
+                                <div class="about-text go-to">
+                                    <h4 class="dark-color">Technologies</h4>
+                                    <p>{holbie.technologies}</p>
+                                </div>
+                                <div class="about-text go-to">
+                                    <h4 class="dark-color">The most amazing thing</h4>
+                                    <p>{holbie.most_amazing_thing}</p>
+                                </div>
+                                <div class="about-text go-to">
+                                    <h4 class="dark-color">Gender</h4>
+                                    <p>{holbie.gender}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
-            </div>
-        </section>
+                    ))}
+                </div>
+            </section>
         </div>
     </Fragment>
 
