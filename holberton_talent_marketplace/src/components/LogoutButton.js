213e3e8d6534b4b34cc6280a4675c83f9a48804e
Juansu01 <thejuank0@hotmail.com>
2021-11-02 09:40:15 -0500

    Fixing position

--- holberton_talent_marketplace/src/components/LogoutButton.js
+++ holberton_talent_marketplace/src/components/LogoutButton.js
@@ -7,8 +7,9 @@ const LogoutButton = () => {
     return (
        <button className="buttonNoStyle nav-item mr-3" onClick={() => logout()}>
 	    Log Out
-        <img src={user.picture} alt={user.name} size="30" height="30" width="30" class="avatar-user avatar avatar-small ml-3"/>
+        <img src={user.picture} alt={user.name} size="25" height="25" width="25" class="avatar-user avatar avatar-small ml-3"/>
        </button>
+
 
     )
 }
