926d52c8752ceced48f0045a4bc5bfb0e6a364f9
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-11-03 18:38:12 -0500

    add others components

--- holberton_talent_marketplace/src/components/ListCompanies.js
+++ holberton_talent_marketplace/src/components/ListCompanies.js
@@ -37,40 +37,46 @@ const ListCompanies = () => {
 	}
 	return (
 		<Fragment>
-        <div class="container myContainer">
-            <h1 className="text-center mt-5">Find A Company</h1>
-            <form>
-                <input type="text" className="form-control form-control-sm mt-5" placeholder="Name, technologies, location" value={q} onChange={(e) => setQ(e.target.value)}></input>
-            </form>
-            <section class="section about-section gray-bg" id="about">
-                <div class="container border">
-                    {search(companies).map(company => (
-                        <Link to={`/company_profile/${company.id}`} style={{ textDecoration: 'none', color: 'black' }}>
-                            <div class="flex-row-reverse border-bottom border-top">
-                                <div class="">
-                                    <div class="about-text go-to">
-                                        <img src={require("../profile_pitures/" + company.id + ".png").default} class="profilePictureCompany img-thumbnail rounded float-end" width="200" height="200" onError={(e) => {
-                                            e.target.src = '../profile_pictures/default_avatar.jpg' // some replacement image
-                                        }} />
-                                        <h3 class="dark-color mt-4">{company.name}</h3>
-                                        <p>{company.about_us.substring(0, 200) + "..."}</p>
-                                    </div>
-                                    <div class="about-text go-to">
-                                        <h4 class="dark-color">Technologies</h4>
-                                        <p>{company.technologies}</p>
-                                    </div>
-                                    <div class="about-text go-to">
-                                        <h4 class="dark-color">Location</h4>
-                                        <p>{company.location}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </Link>
-                    ))}
-                </div>
-            </section>
-        </div>
-    </Fragment>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-3"></div>
+					<div class="col-7">
+						<h1 className="text-center mt-5">Find A Company</h1>
+						<form>
+							<input type="text" className="form-control form-control-sm mt-5" placeholder="Name, technologies, location" value={q} onChange={(e) => setQ(e.target.value)}></input>
+						</form>
+						<section class="section about-section gray-bg" id="about">
+							<div class="container border">
+								{search(companies).map(company => (
+									<Link to={`/company_profile/${company.id}`} style={{ textDecoration: 'none', color: 'black' }}>
+										<div class="flex-row-reverse border-bottom border-top">
+											<div class="">
+												<div class="about-text go-to">
+													<img src={require("../profile_pitures/" + company.id + ".png").default} class="profilePictureCompany img-thumbnail rounded float-end" width="200" height="200" onError={(e) => {
+														e.target.src = '../profile_pictures/default_avatar.jpg' // some replacement image
+													}} />
+													<h3 class="dark-color mt-4">{company.name}</h3>
+													<p>{company.about_us.substring(0, 200) + "..."}</p>
+												</div>
+												<div class="about-text go-to">
+													<h4 class="dark-color">Technologies</h4>
+													<p>{company.technologies}</p>
+												</div>
+												<div class="about-text go-to">
+													<h4 class="dark-color">Location</h4>
+													<p>{company.location}</p>
+												</div>
+											</div>
+										</div>
+									</Link>
+								))}
+							</div>
+						</section>
+					</div>
+					<div class="col-6"></div>
+				</div>
+			</div>
+		</Fragment>
 	)
 
 }
